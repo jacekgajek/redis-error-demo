4014e8fd7ec4f31856c2ff659b52f22c03a8b8a7
Jacek Gajek <jacek.gajek@codibly.com>
2021-12-16 12:19:25 +0100

    suspend

--- src/main/kotlin/com/example/BarController.kt
+++ src/main/kotlin/com/example/BarController.kt
@@ -4,22 +4,30 @@ import io.micronaut.cache.annotation.CacheConfig
 import io.micronaut.cache.annotation.Cacheable
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
-import mu.KotlinLogging
+import kotlinx.coroutines.delay
+import java.io.Serializable
 import java.time.LocalDateTime
-import kotlin.reflect.KClass
 
 @Controller
-@CacheConfig("bar")
+@CacheConfig("default")
 open class BarController {
 
-    data class Bar(val bar: String)
+    data class Bar(val bar: String) : Serializable
 
     private val log = logger(BarController::class)
 
     @Get(uri = "/bar")
     @Cacheable
-    open fun foo(): Bar {
+    open fun bar2(): Bar {
         log.info { "Barring" }
+        return Bar(LocalDateTime.now().toString())
+    }
+
+    @Get(uri = "/bar-suspend")
+    @Cacheable
+    open suspend fun bar(): Bar {
+        log.info { "Barring sus" }
+        delay(5000)
         return Bar(LocalDateTime.now().toString())
     }
 }
--- src/main/kotlin/com/example/FooController.kt
+++ src/main/kotlin/com/example/FooController.kt
@@ -4,22 +4,31 @@ import io.micronaut.cache.annotation.CacheConfig
 import io.micronaut.cache.annotation.Cacheable
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import kotlinx.coroutines.delay
 import mu.KotlinLogging
+import java.io.Serializable
 import java.time.LocalDateTime
 import kotlin.reflect.KClass
 
 @Controller
-@CacheConfig("foo")
+@CacheConfig("default")
 open class FooController {
 
-    data class Foo(val foo: String)
+    data class Foo(val foo: String) : Serializable
 
     private val log = logger(FooController::class)
 
     @Get(uri = "/foo")
     @Cacheable
-    open fun foo(): Foo {
-        log.info { "Fooooooiing" }
-        return Foo(LocalDateTime.now().toString())
+    open fun foo2(param: String): Foo {
+        log.info { "Fooooooiing $param" }
+        return Foo(param + ": " + LocalDateTime.now().toString())
+    }
+    @Get(uri = "/foo-suspend")
+    @Cacheable
+    open suspend fun foo(param: String): Foo {
+        log.info { "Fooooooiing sus $param" }
+        delay(5000)
+        return Foo(param + ": " + LocalDateTime.now().toString())
     }
 }
